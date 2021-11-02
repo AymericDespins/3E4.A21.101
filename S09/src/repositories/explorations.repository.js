5135b6317c4a5991a26637c6bdc3030d52588e01
Aymeric Despins <1652191@cstj.qc.ca>
2021-11-02 09:31:23 -0400

    fin cours 2021-11-02

---
+++ S09/src/repositories/explorations.repository.js
@@ -0,0 +1,48 @@
+import Exploration from '../models/exploration.model.js';
+import planetRepository from './planet.repository.js';
+
+class ExplorationsRepository {
+
+    retrieveAll(retrieveOptions) {
+        const retrieveQuery = Exploration.find().skip(retrieveOptions.skip).limit(retrieveOptions.limit);
+        const countQuery = Exploration.countDocuments();
+
+        return Promise.all([retrieveQuery, countQuery]);
+    }
+
+    retrieveById(idExploration, retrieveOptions) {
+
+        const retrieveQuery = Exploration.findById(idExploration);
+
+        if(retrieveOptions.planet)
+        {
+            retrieveQuery.populate('planet');
+        }
+
+        return retrieveQuery;
+    }
+
+    transform(exploration, transformOptions = {}) {
+
+        if(transformOptions.embed && transformOptions.embed.planet) {
+            //?embed=planet
+            // exploration.planet => un objet complet
+            exploration.planet = planetRepository.transform(exploration.planet, transformOptions);
+        } else {
+            //On ne veut pas embed la planet
+            // exploration.planet => seulement le id
+            exploration.planet = { href: `/planets/${exploration.planet}`};
+        }
+
+        //Choix pour le BASE_URL
+        //exploration.href = `${process.env.BASE_URL}/explorations/${exploration._id}`;
+        exploration.href = `/explorations/${exploration._id}`;
+
+        delete exploration._id;
+
+        return exploration;
+    }
+    
+}
+
+export default new ExplorationsRepository();
---
+++ S09/src/routes/explorations.routes.js
@@ -0,0 +1,88 @@
+import express, { response } from 'express';
+import paginate from 'express-paginate';
+import HttpError from 'http-errors';
+
+import explorationsRepository from '../repositories/explorations.repository.js';
+
+const router = express.Router(); 
+
+class ExplorationsRoutes {
+    
+    constructor() {
+        router.get('/', paginate.middleware(20, 50), this.getAll);
+        router.get('/:explorationId', this.getOne);
+    }
+
+    async getAll(req, res, next) {
+
+        const retrieveOptions = {
+            skip: req.skip,
+            limit: req.query.limit
+        }
+        
+        try {
+            let [explorations, itemsCount] = await explorationsRepository.retrieveAll(retrieveOptions);
+            const pageCount = Math.ceil(itemsCount / req.query.limit);
+            const hasNextPage = paginate.hasNextPages(req)(pageCount);
+
+            explorations = explorations.map(e => {
+                e = e.toObject({getters: false, virtuals: false});
+                e = explorationsRepository.transform(e);
+                return e;
+            });
+
+            const response = {
+                _metadata:{
+                    hasNextPage: hasNextPage,
+                    page : req.query.page,
+                    limit: req.query.limit,
+                    skip: req.skip,
+                    totalPages: pageCount,
+                    totalDocuments: itemsCount
+                },
+                _links:{
+                    
+                },
+                data: explorations
+            };
+
+            res.status(200).json(response);
+
+        } catch(err) {
+            return next(err);
+        }
+    }
+
+    async getOne(req, res, next) {
+
+        const retrieveOptions = {};
+        const transformOptions = { embed:{} };
+
+        if(req.query.embed && req.query.embed === 'planet') {
+            retrieveOptions.planet = true;
+            transformOptions.embed.planet = true;
+        }
+
+        try {
+            const idExploration = req.params.explorationId;
+            let exploration = await explorationsRepository.retrieveById(idExploration, retrieveOptions);
+            if(!exploration) {
+                return next(HttpError.NotFound());
+            }
+
+            exploration = exploration.toObject({getters:false, virtuals:false});
+            exploration = explorationsRepository.transform(exploration, transformOptions);
+
+            res.status(200).json(exploration);
+
+
+        } catch(err) {
+            return next(err);
+        }
+    }
+
+}
+
+new ExplorationsRoutes();
+
+export default router;
